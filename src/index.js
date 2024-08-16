4805baf5e4bb31efd421e6abaa8a98c17374cf55
dwirx <bangundwir@gmail.com>
2024-08-17 00:43:51 +0700

    update tranlte text

--- src/index.js
+++ src/index.js
@@ -16,14 +16,3 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((registration) => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      })
-      .catch((error) => {
-        console.log('ServiceWorker registration failed: ', error);
-      });
-  });
-}
